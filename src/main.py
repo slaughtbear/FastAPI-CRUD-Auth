ce525b6efb48e81ee9f1e8b9afcedb5716b186d6
slaughtbear <ivanaguirre368@gmail.com>
2025-01-08 19:11:07 -0600

    Add users router

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.routes.albums import albums_router
+from src.routes.users import users_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ async def main():
     return {"message": "Hello World"}
 
 app.include_router(albums_router, prefix="/albums")
+app.include_router(users_router, prefix="/users")
