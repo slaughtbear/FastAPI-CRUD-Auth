e0da112ab20934389a8a41ec0d1334ccbc50fb26
slaughtbear <ivanaguirre368@gmail.com>
2025-01-13 09:11:24 -0600

    Add endpoint to get users by id

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter, HTTPException, Path, Query
 from src.models.users import User, users_list
-from typing import List
+from typing import Union, List
+
 
 users_router = APIRouter()
 
-@users_router.get("/", response_model=List[User], tags=["Users"])
+
+@users_router.get("/", response_model=List[User])
 async def get_users() -> List[User]:
     return users_list
+
+
+@users_router.get("/{id}", response_model=User)
+async def get_user(id: int = Path(gt=0)) -> User:
+    user = search_user(id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return user
+
+
+def search_user(id: int) -> Union[User, None]:
+    return next((user for user in users_list if user.id == id), None)
